93c24828f8756d8e2a0a2acb51f6842fb6dda0fb
AliaxandraKasich <Sane4ka022@mail.ru>
2018-02-21 20:24:24 +0300

    my commit

--- src/index.js
+++ src/index.js
@@ -1,30 +1,43 @@
 class Sorter {
   constructor() {
-    // your implementation
+   this.arr = [];
+   this.comparator = (a,b) => a-b;
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    var tempArr = [];
+    for(var i=0; i<this.arr.length;i++){
+      tempArr.push(this.arr[i]);
+    }
+    return tempArr;
   }
 
   sort(indices) {
-    // your implementation
+    var tempArr = [];
+    for(var i=0; i<indices.length;i++){
+      tempArr.push(this.arr[indices[i]]);
+    }
+    tempArr.sort(this.comparator);
+    for(var i=0; i<indices.length;i++){
+      this.arr.splice(indices[i],1,tempArr[i]);
+    }
+    
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
